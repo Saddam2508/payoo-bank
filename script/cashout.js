7a92beb9f581222e4dca4616f5baf831029bf132
saddam2508 <eyarinsultana@gmail.com>
2026-02-18 18:06:06 +0600

    cashout added

--- script/cashout.js
+++ script/cashout.js
@@ -1,10 +1,33 @@
 document.getElementById('cashout-btn').addEventListener('click', function () {
-  console.log('first');
   //1.get the agent number & validate
+  const agentNum = getValue('agent-number');
+  console.log(agentNum);
   //2.get the amount, validate, convert to Number
+  const amount = getValue('cashout-amount');
+  const amountConvert = Number(amount);
+  console.log(amountConvert);
   //3.get the current balance, validate, convert to Number
+  const balance = getText('balance');
+  const balanceConvert = Number(balance.slice(1));
+  console.log(balanceConvert);
   //4.calculate new balance
+  const newBalance = balanceConvert - amountConvert;
+
+  console.log(newBalance);
   //5.get the pin and verify
+  const pin = getValue('cashout-pin');
+  console.log(pin);
+  if (pin !== '1234') {
+    return alert('Invalid pin number');
+  }
   //6.true:: show and alert > set Balance
+  if (newBalance < 0) {
+    alert('Invalid amount ');
+    return;
+  } else {
+    alert('withdraw success');
+    const setBalance = document.getElementById('balance');
+    setBalance.innerText = `$${newBalance}`;
+  }
   //7.true:: show and error alert > return
 });
--- script/helpers.js
+++ script/helpers.js
@@ -3,3 +3,8 @@ function getValue(id) {
   const value = input.value;
   return value;
 }
+function getText(id) {
+  const input = document.getElementById(id);
+  const value = input.innerText;
+  return value;
+}
