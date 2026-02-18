c4ff9d0b0b9adb0ed8f4c103cc107e3e10ee34e2
saddam2508 <eyarinsultana@gmail.com>
2026-02-18 15:04:39 +0600

    cashout and homepage in progress...

---
+++ script/cashout.js
@@ -0,0 +1,10 @@
+document.getElementById('cashout-btn').addEventListener('click', function () {
+  console.log('first');
+  //1.get the agent number & validate
+  //2.get the amount, validate, convert to Number
+  //3.get the current balance, validate, convert to Number
+  //4.calculate new balance
+  //5.get the pin and verify
+  //6.true:: show and alert > set Balance
+  //7.true:: show and error alert > return
+});
--- script/login.js
+++ script/login.js
@@ -1,16 +1,16 @@
 document.getElementById('login-btn').addEventListener('click', function () {
   const mobileNumber = getValue('mobile-number');
-  if (mobileNumber !== '01535212168') {
+  if (mobileNumber !== '01234567891') {
     alert(' Invalid number');
     return;
-    }
-    
+  }
+
   const pin = getValue('pin');
   if (pin !== '1234') {
     alert('Invalid pin');
     return;
   } else {
     alert('login success');
-    window.location.replace('../home.html');
+    window.location.assign('../home.html');
   }
 });
